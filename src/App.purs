7e4d6c918d8865d4f41d70072c355b199577fd50
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-03-24 19:05:47 +0300

    feat: Add display for sudoku board

--- src/App.purs
+++ src/App.purs
@@ -1,66 +1,69 @@
-module App where
+module App (component) where
 
 import Prelude
 
 import Data.Int (decimal, toStringAs)
+import Data.Maybe (Maybe(..))
 import Effect.Class (class MonadEffect)
+import Halogen (ClassName(..))
 import Halogen as H
 import Halogen.HTML (ClassName(..))
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
-type State = { count :: Int }
+type State =
+  { puzzle :: Array (Array (Maybe Int))
+  }
 
 initialState :: forall input. input -> State
-initialState _ = { count: 0 }
+initialState _ =
+  { puzzle:
+      [ [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      , [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+      ]
+  }
 
-data Action = Increment | Decrement
+data Action
 
 handleAction :: forall o m. MonadEffect m => Action -> H.HalogenM State Action () o m Unit
-handleAction = case _ of
-  Increment -> H.modify_ \{ count } -> { count: count + 1 }
-  Decrement -> H.modify_ \{ count } -> { count: count - 1 }
+handleAction _ = pure unit
 
 render :: forall cs m. State -> H.ComponentHTML Action cs m
-render { count } =
+render { puzzle } =
   HH.div
-    [ HP.class_ $ ClassName "w-5/6 mx-auto" ]
-    [ HH.h1
-        [ HP.class_ $ ClassName "text-center text-3xl font-semibold p-4" ]
-        [ HH.text "Home page" ]
-    , HH.div
-        []
-        [ HH.h2
-            [ HP.class_ $ ClassName "text-center text-lg p-4" ]
-            [ HH.text "Purescript, Typescript and Tailwind template" ]
-        , HH.p
-            [ HP.class_ $ ClassName "text-lg px-4 text-center" ]
-            [ HH.text "A template for a Web App Written in purescript using the halogen framework" ]
-        , HH.div
-            [ HP.class_ $ ClassName "w-4/6 mx-auto text-center py-8" ]
-            [ HH.span
-                [ HP.class_ $ ClassName "text-xl px-4" ]
-                [ HH.text "Counter: " ]
-            , HH.span
-                [ HP.class_ $ ClassName "text-4xl px-4" ]
-                [ HH.text $ toStringAs decimal count ]
-            ]
-        , HH.div
-            [ HP.class_ $ ClassName "flex justify-center space-x-8" ]
-            [ HH.button
-                [ HP.class_ $ ClassName "text-white text-center text-xl p-2 bg-gray-400 rounded-xl"
-                , HE.onClick \_ -> Increment
-                ]
-                [ HH.text "Increment" ]
-            , HH.button
-                [ HP.class_ $ ClassName "text-white text-center text-xl p-2 bg-gray-400 rounded-xl"
-                , HE.onClick \_ -> Decrement
-                ]
-                [ HH.text "Decrement" ]
-            ]
-        ]
+    [ HP.class_ $ ClassName $ "absolute h-96 w-96 my-auto mx-auto border-2 border-slate-500 flex flex-col "
+        <> " inset-1/4"
     ]
+    ( map
+        ( \row -> HH.div
+            [ HP.id "row"
+            , HP.class_ $ ClassName "flex flex-row flex-1"
+            ]
+            (map renderCell row)
+
+        )
+        puzzle
+    )
+  where
+  renderCell cell =
+    HH.div
+      [ HP.class_ $ ClassName "border-2 border-slate-100"
+      ]
+      [ case cell of
+          Just v -> HH.text $ toStringAs decimal v
+          Nothing ->
+            ( HH.input
+                [ HP.class_ $ ClassName "w-full h-full"
+                ]
+            )
+      ]
 
 component :: forall q o m. MonadEffect m => H.Component q Unit o m
 component = H.mkComponent
